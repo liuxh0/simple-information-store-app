598ad77060313bda0ced51d9bc6900cf9e680cc3
Xinhu Liu <xinhu.liu90@gmail.com>
2021-05-09 00:33:02 +0200

    Add integration tests in GitHub Actions (#1)

--- scripts/init-local-dynamodb.sh
+++ scripts/init-local-dynamodb.sh
@@ -1,2 +1,2 @@
-docker run -d -p 8000:8000 amazon/dynamodb-local
-aws dynamodb create-table --cli-input-json file://local-dynamodb-value-table.json --endpoint-url http://localhost:8000
+docker run --name dynamodb --network sam -p 8000:8000 -d amazon/dynamodb-local
+aws dynamodb create-table --cli-input-json file://local-dynamodb-value-table.json --endpoint-url http://localhost:8000 --no-cli-pager
